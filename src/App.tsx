958742977de246481a3649629756972b3b70c216
Byron Powell <ByronP40@gmail.com>
2026-03-27 00:11:50 +0000

    updated main and app2

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import SignIn from "@/pages/SignIn";
 import SignUp from "@/pages/SignUp";
 import NotFound from "@/pages/NotFound";
 
-function App() {
+export default function App() {
   return (
     <TooltipProvider>
       <Toaster />
@@ -28,13 +28,19 @@ function App() {
 
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/contractorDashboard" element={<ContractorDashboard />} />
-        <Route path="/complete-contractor-profile" element={<CompleteContractorProfile />} />
+        <Route
+          path="/complete-contractor-profile"
+          element={<CompleteContractorProfile />}
+        />
 
         <Route path="/create-job" element={<CreateJob />} />
         <Route path="/jobs/:jobId" element={<JobDetail />} />
         <Route path="/jobs/:jobId/quotes" element={<JobQuotes />} />
 
-        <Route path="/contractor-dashboard" element={<Navigate to="/contractorDashboard" replace />} />
+        <Route
+          path="/contractor-dashboard"
+          element={<Navigate to="/contractorDashboard" replace />}
+        />
 
         <Route path="*" element={<NotFound />} />
       </Routes>
